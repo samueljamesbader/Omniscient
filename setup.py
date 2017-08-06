4fef10e83de42c38bdb507f64ca8574f2fff6af3
samueljamesbader <samuel.james.bader@gmail.com>
2017-08-06 01:52:59 -0400

    Trying conda build again

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
-print("No set-up necessary!")
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='Omniscient',
+      version='0.0.2',
+      description='Python package for reading and accessing information in complex nested structures (eg. material parameters)',
+      author='Samuel James Bader',
+      author_email='samuel.james.bader@gmail.com',
+      url='https://github.com/samueljamesbader/Omniscient',
+      packages=['omniscient']
+)
