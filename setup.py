089602c7dc48bbb9c2d1f1b10879d2a0ddc02f2e
samueljamesbader <samuel.james.bader@gmail.com>
2017-08-06 00:41:16 -0400

    Prepping for v0.0.1 to test conda-build

---
+++ setup.py
@@ -0,0 +1,1 @@
+print("No set-up necessary!")
---
+++ tests/test_omniscient.py
@@ -0,0 +1,15 @@
+#import pytest
+#
+#if __name__=="__main__": pytest.main(args=[__file__,'-s'])
+
+import os.path
+if __name__=='__main__':
+    folder=os.path.join(os.path.dirname(__file__),'knowledge')
+
+
+    from omniscient.omniscient import Brain
+    brain=Brain(folder)
+
+    brain.read("VM2003.txt")
+    print(brain('GaN.relaxed.varshni.Eg0'))
+
